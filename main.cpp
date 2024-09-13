350929933f991abefb28d6533d947bd9db801739
shenj68 <shenj68@mcmaster.ca>
2024-09-13 15:32:47 -0400

    just a testing ground for .cmake files

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hej! Code is running." << std::endl;
+
+    return 0;
+}
---
+++ testing.cmake
@@ -0,0 +1,3 @@
+if(EXISTS "${ROOT}/etc/nv_tegra_release")
+    message(STATUS "[PORTFILE] - Jetson NVIDIA Tegra Release FOUND")
+endif()
