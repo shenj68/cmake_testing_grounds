b665fddffa544c006f423a15b05428fb41f10bae
shenj68 <shenj68@mcmaster.ca>
2024-09-26 12:18:19 -0400

    untested: differentiate between WSL, Jetsons and Windows

--- testing.cmake
+++ testing.cmake
@@ -1,57 +1,109 @@
-# Check if the system is Linux
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    # Try to detect Ubuntu specifically
-    message(STATUS "[PORTFILE] - Detected Linux")
-    execute_process(
-        COMMAND lsb_release -i
-        OUTPUT_VARIABLE DISTRO_ID
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-    )
+# Find the LSB (Linux Standard Base) (Determine if host is using Ubuntu) 
+find_program(LSB_RELEASE_EXEC lsb_release)
+execute_process(COMMAND ${LSB_RELEASE_EXEC} -is
+    OUTPUT_VARIABLE Linux_Standard_Base
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+message (STATUS "[PORTFILE] - Linux_Standard_Base: ${Linux_Standard_Base}")
 
-    if(DISTRO_ID MATCHES "Ubuntu")
-        message(STATUS "Detected Ubuntu Linux")
-        # Add your specific configuration for Ubuntu here
-    else()
-        message(FATAL_ERROR "This port is only supported on Ubuntu Linux.")
-    endif()
+
+# UbuntuFlag
+if (Linux_Standard_Base MATCHES "Ubuntu")
+    message(STATUS "[PORTFILE] - Ubuntu Detected")
+    set(ubuntu_flag 1)
 else()
-    message(FATAL_ERROR "This port is only supported on Linux.")
-endif()
-
-message(STATUS "CMAKE_HOST_SYSTEM_VERSION: ${CMAKE_HOST_SYSTEM_VERSION}")
-
-if(CMAKE_HOST_SYSTEM_VERSION MATCHES "microsoft-standard-WSL2")
-    set(boop 1)
-endif()
-
-if(boop MATCHES 1)
-    message(STATUS "QLKWELQWELWQELELE")
-endif()
-
-if(boop MATCHES 2)
-    message(STATUS "LOLOLOL ITS 2")
+    message(STATUS "[PORTFILE] - Ubuntu NOT Detected")
+    set(ubuntu_flag 0)
 endif()
 
 
-set(boop2 "microsoft")
-
-if ( CMAKE_HOST_SYSTEM_VERSION MATCHES "microsoft-standard-WSL2") 
-    set(wsl_condition_cnt_1 1)
+# WSLFlag
+if ( CMAKE_HOST_SYSTEM_VERSION MATCHES "microsoft-standard-WSL2" OR CMAKE_HOST_SYSTEM_VERSION MATCHES "microsoft-standard-WSL") 
+    set(wsl_flag 1)
+    #message("wsl_flag: ${wsl_flag}")
+else()
+    set(wsl_flag 0)
+    #message("wsl_flag: ${wsl_flag}")
 endif()
+
+
+# Linux Flag
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    set (wsl_condition_cnt_2 1)
-endif()
-if (CMAKE_HOST_SYSTEM_VERSION MATCHES "tegra")
-    set (wsl_condition_cnt_3 1)
+    set(linux_flag 1)
+    #message("linux_flag: ${linux_flag}")
 else()
-    set(wsl_condition_cnt_3 0)
+    set(linux_flag 0)
+    #message("linux_flag: ${linux_flag}")
 endif()
 
-# AND (CMAKE_SYSTEM_NAME STREQUAL "Linux") AND (CMAKE_HOST_SYSTEM_VERSION NOT MATCHES "tegra")
-#     message(STATUS "YEEEETT BABY")
-# endif()
 
-if( (wsl_condition_cnt_1 MATCHES 1) AND (wsl_condition_cnt_2 MATCHES 1) AND (wsl_condition_cnt_3 MATCHES 0) )
-    message(STATUS "YEEEET")
+# JetsonFlag/Tegra Devices
+if (CMAKE_HOST_SYSTEM_VERSION MATCHES "tegra" AND EXISTS "${ROOT}/etc/nv_tegra_release")
+    set(jetson_flag 1)
+    #message("jetson_flag: ${jetson_flag}")
+else()
+    set(jetson_flag 0)
+    #message("jetson_flag: ${jetson_flag}")
+endif()
+
+
+# WindowsFlag
+if (CMAKE_HOST_SYSTEM_NAME MATCHES "Windows" AND CMAKE_HOST_SYSTEM_VERSION MATCHES "10.0")
+    set(windows_flag 1)
+    #message("windows_flag: ${windows_flag}")
+else()
+    set(windows_flag 0)
+    #message("windows_flag: ${windows_flag}")
+endif()
+
+
+# [WSL2] - Supported
+if( (wsl_flag MATCHES 1) 
+    AND (linux_flag MATCHES 1) 
+    AND (ubuntu_flag MATCHES 1)
+    AND (jetson_flag MATCHES 0) 
+    AND (windows_flag MATCHES 0))
+    message(STATUS "[PORTFILE] - WSL Detected")
+endif()
+
+
+# [Linux + Ubuntu] - Supported
+if( (wsl_flag MATCHES 0) 
+    AND (linux_flag MATCHES 1) 
+    AND (ubuntu_flag MATCHES 1)
+    AND (jetson_flag MATCHES 0) 
+    AND (windows_flag MATCHES 0) )
+    message(STATUS "[PORTFILE] - Linux + Ubuntu Machine Detected")
+endif()
+
+
+# [Nvidia Devices] - Supported
+if( (wsl_flag MATCHES 0) 
+    AND (linux_flag MATCHES 1)
+    AND (ubuntu_flag MATCHES 1) 
+    AND (jetson_flag MATCHES 1) 
+    AND (windows_flag MATCHES 0))
+    message(STATUS "[PORTFILE] - Nvidia Jetson Device Detected")
+endif()
+
+
+# [Windows Devices] - Not Supported
+if( (wsl_flag MATCHES 0) 
+    AND (linux_flag MATCHES 0)
+    AND (ubuntu_flag MATCHES 0) 
+    AND (jetson_flag MATCHES 0) 
+    AND (windows_flag MATCHES 1))    
+    message(STATUS "[PORTFILE] - Windows Detected")
+    message(FATAL_ERROR "[PORTFILE] - !!! WINDOWS NOT SUPPORTED, ABORTING... !!!")
+endif()
+
+
+# Not Currently Supported
+if( (wsl_flag MATCHES 0) 
+    AND (ubuntu_flag MATCHES 0)
+    AND (linux_flag MATCHES 0) 
+    AND (jetson_flag MATCHES 0) 
+    AND (windows_flag MATCHES 0))    
+    message(STATUS "[PORTFILE] - NONE Detected")
+    message(FATAL_ERROR "[PORTFILE] - !!! ARCHITECTURE NOT SUPPORTED, ABORTING... !!!")
 endif()
